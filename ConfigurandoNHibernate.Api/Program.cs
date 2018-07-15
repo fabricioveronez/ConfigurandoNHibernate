3f0894e434d5f484470178de86301438697259c1
DESKTOP-OL596BN\fabri <fabricioveronez@gmail.com>
2018-07-15 20:18:57 -0300

    Inserindo a migration no início do projeto.

--- ConfigurandoNHibernate.Api/Program.cs
+++ ConfigurandoNHibernate.Api/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using ConfigurandoNHibernate.Migration;
 
 namespace ConfigurandoNHibernate.Api
 {
@@ -14,7 +11,32 @@ namespace ConfigurandoNHibernate.Api
     {
         public static void Main(string[] args)
         {
+            var serviceProvider = CreateServices();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddMySql5()
+                    .WithGlobalConnectionString("Server=localhost;Database=wordpress;Uid=fsvblog;Pwd=fsvblogpass;SslMode=none;")
+                    .ScanIn(typeof(CriacaoTabelaFuncionario).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        public static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
